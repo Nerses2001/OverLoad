e635a7bebc7afe6756993434742fd938673f0d5e
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-07-31 09:54:01 -0800

    OverLoad where select

---
+++ OverLoad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OverLoad
+{
+
+ 
+        internal static class OverLoad
+        {
+           public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            Console.WriteLine("OverLoad Where");
+            return System.Linq.Enumerable.Where(source, predicate);
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            Console.WriteLine("OverLoad Select");
+            return System.Linq.Enumerable.Select(source, selector);
+        }
+
+        }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace OverLoad
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] number = { 1, 5, 6, 9, 2, 5 };
+            var query = from n in number
+                        where n > 3
+                        select n.ToString();
+      
+       
+            Console.ReadKey();  
+        
+        }
+
+      
+    }
+}
